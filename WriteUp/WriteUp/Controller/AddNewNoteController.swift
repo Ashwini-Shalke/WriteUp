766dc78a20f093b8de642a616f709643dc4cb0a4
Ashwini shalke <ashwinishalke050@gmail.com>
2020-06-01 14:58:06 +0530

    fix the UITextView of AddNewNote Layout

--- WriteUp/WriteUp/Controller/AddNewNoteController.swift
+++ WriteUp/WriteUp/Controller/AddNewNoteController.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 class AddNewNoteController: UIViewController {
+
+    
+    
+    var textfieldHeight:NSLayoutConstraint?
+    var textfieldHeightWithKeyBoard:CGFloat?
+    var textfieldHeightWithoutKeyBoard:CGFloat = (UIScreen.main.bounds.height - 150)
     
     let textView: UITextView = {
         let textview = UITextView()
         textview.isEditable = true
         textview.font = UIFont.systemFont(ofSize: 20)
+        textview.backgroundColor = .systemGray6
         return textview
     }()
     
@@ -25,9 +32,19 @@ class AddNewNoteController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(handleNewNote))
         
         view.addSubview(textView)
-        
-        textView.backgroundColor = .green
-        textView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: UIEdgeInsets(top: 16, left: 16, bottom: 0 , right: -16), size: CGSize(width: 0, height: 300))
+        textfieldHeight = NSLayoutConstraint(item: textView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: textfieldHeightWithoutKeyBoard)
+
+        textView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                        leading: view.safeAreaLayoutGuide.leadingAnchor,
+                        bottom: nil,
+                        trailing: view.safeAreaLayoutGuide.trailingAnchor,
+                        padding: UIEdgeInsets(top: 16,
+                                              left: 16,
+                                              bottom: 0 ,
+                                              right: -16))
+        NSLayoutConstraint.activate([
+            textfieldHeight!
+        ])
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
@@ -42,25 +59,20 @@ class AddNewNoteController: UIViewController {
         guard let userInfo = notification.userInfo else { return }
         guard let keyboardSize = userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue else { return }
         let keybardFrame = keyboardSize.cgRectValue
-        let keyboardYaxis = self.view.frame.size.height - keybardFrame.height
-        print("keyboardhieght", keyboardYaxis)
-        
-        let editTextFieldY: CGFloat = textView.frame.maxY
-        print("textView maxY", editTextFieldY)
-        if self.view.frame.origin.y >= 0 {
-            if editTextFieldY > keyboardYaxis {
-                UIView.animate(withDuration: 0.25, delay: 0, options: UIView.AnimationOptions.curveEaseIn, animations: {
-                    self.textView.anchor(top: self.view.safeAreaLayoutGuide.topAnchor, leading: self.view.safeAreaLayoutGuide.leadingAnchor, bottom: self.view.safeAreaLayoutGuide.bottomAnchor, trailing: self.view.safeAreaLayoutGuide.trailingAnchor, padding: UIEdgeInsets(top: 16, left: 16, bottom: -keybardFrame.height - 50 , right: -16))
-                }, completion: nil)
-            }
-        }
+        let textfieldHeightWithKeyBoard = ((textfieldHeightWithoutKeyBoard) - keybardFrame.height)
+
+        UIView.animate(withDuration: 0,delay: 0,options: UIView.AnimationOptions.curveEaseIn,
+                       animations: {
+                        self.textfieldHeight?.constant = textfieldHeightWithKeyBoard
+                        self.view.layoutIfNeeded()
+        }, completion: nil)
     }
     
     @objc func keyboardWillHide(notification: NSNotification){
-       UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
-            self.view.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
-        self.textView.anchor(top: self.view.safeAreaLayoutGuide.topAnchor, leading: self.view.safeAreaLayoutGuide.leadingAnchor, bottom: self.view.safeAreaLayoutGuide.bottomAnchor, trailing: self.view.safeAreaLayoutGuide.trailingAnchor, padding: UIEdgeInsets(top: 16, left: 16, bottom: -16 , right: -16))
-        }, completion: nil)
+       UIView.animate(withDuration: 0, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
+        self.textfieldHeight?.constant = self.textfieldHeightWithoutKeyBoard
+        self.view.layoutIfNeeded()
+       }, completion: nil)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
