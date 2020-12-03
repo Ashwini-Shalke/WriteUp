2287a08b5432d8214279b35890f51c7598f4dc07
Ashwini shalke <ashwinishalke050@gmail.com>
2020-12-03 11:52:28 +0530

    refactor ProfileDetail for readability

---
+++ WriteUp/WriteUp/View/SaveNoteView.swift
@@ -0,0 +1,16 @@
+//
+//  SaveNoteView.swift
+//  WriteUp
+//
+//  Created by Ashwini shalke on 02/12/20.
+//  Copyright © 2020 Ashwini Shalke. All rights reserved.
+//
+
+import UIKit
+
+class SaveNoteView : BaseView {
+    
+    override func setup() {
+    
+    }
+}
